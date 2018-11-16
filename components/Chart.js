3a92ba8746456791ee2b089dae27fa683ed11c8f
akhiljasrotia <abujasrotia@gmail.com>
2018-11-16 09:45:07 +0530

    Added: Charts

--- components/Chart.js
+++ components/Chart.js
@@ -1,35 +1,69 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { LineChart, Grid } from 'react-native-svg-charts';
+import { View, Text, StyleSheet,ScrollView } from 'react-native';
+import { LineChart, Grid ,PieChart } from 'react-native-svg-charts';
 
 class Chart extends Component {
 
 	constructor(props) {
-		
+		super(props);
 	}
 
 	render() {
 		const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+
+		const randomColor = () => ('#' + (Math.random() * 0xFFFFFF << 0).toString(16) + '000000').slice(0, 7)
+
+        const pieData = data
+            .filter(value => value > 0)
+            .map((value, index) => ({
+                value,
+                svg: {
+                    fill: randomColor(),
+                    onPress: () => console.log('press', index),
+                },
+                key: `pie-${index}`,
+            }))
+
 		return (
-			<View>
-				<LineChart
-	                style={{ height: 200 }}
-	                data={ data }
-	                svg={{ stroke: 'rgb(134, 65, 244)' }}
-	                contentInset={{ top: 20, bottom: 20 }}
-	            >
-	                <Grid/>
-	            </LineChart>
-            </View>
+			<ScrollView>
+				<View style={styles.mainContainer}>
+					<View style={styles.heading}>
+						<Text style={styles.headingText}>Face Count and Analysis</Text>
+					</View>
+					<LineChart
+		                style={{ height: 200,width:350}}
+		                data={ data }
+		                svg={{ stroke: 'rgb(134, 65, 244)' }}
+		                contentInset={{ top: 20, bottom: 20 }}
+		            >
+		                <Grid/>
+		            </LineChart>
+		            <View style={styles.heading}>
+						<Text style={styles.headingText}>Sentiment Analysis</Text>
+					</View>
+		            <PieChart
+	                style={ { height: 200 ,width:200 } }
+	                data={ pieData }
+	            	/>
+	            </View>
+            </ScrollView>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
-	test: {
+	mainContainer: {
 		flex: 1,
 		justifyContent: 'center',
 	    alignItems: 'center',
+	    padding:15,
+	},
+	heading:{
+		paddingVertical:10,
+	},
+	headingText:{
+		textAlign:'center',
+		fontSize:20,
 	}
 });
 
